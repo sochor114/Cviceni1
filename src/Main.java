cfeca5245827b09af6f299c85596ab131aa49a98
Sochost1 <sochor112@seznam.cz>
2023-02-14 10:27:18 +0100

    hello 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        System.out.println("Hello 2 ");
     }
+
 }
